7d33c30bb814c1e6d2fbb73ea32cff2efd6da5cb
larcenouardi <contact.ouardilahcen@gmail.com>
2023-07-27 23:16:35 +0100

    edit

--- public/js/Script_HK.js
+++ public/js/Script_HK.js
@@ -1,8 +1,10 @@
 const formulaire = document.getElementById("formulaire");
 const listeDepenses = document.getElementById("liste-depenses");
 const total = document.getElementById("total");
+const exportButton = document.getElementById("export-button");
 
 let depenses = [];
+let totalDepenses = 0;
 
 function ajouterDepense(description, montant) {
   const depense = {
@@ -10,11 +12,12 @@ function ajouterDepense(description, montant) {
     montant: parseFloat(montant),
   };
   depenses.push(depense);
+  totalDepenses += depense.montant;
 }
 
 function afficherDepenses() {
   listeDepenses.innerHTML = "";
-  depenses.forEach(function (depense) {
+  depenses.forEach(function (depense, index) {
     const ligne = document.createElement("tr");
 
     const description = document.createElement("td");
@@ -25,27 +28,79 @@ function afficherDepenses() {
     montant.textContent = depense.montant.toFixed(2) + " DH";
     ligne.appendChild(montant);
 
+    const actions = document.createElement("td");
+actions.classList.add("actions");
+    const modifierButton = document.createElement("button");
+    modifierButton.textContent = "Modifier";
+    modifierButton.addEventListener("click", function () {
+      modifierDepense(index);
+    });
+    actions.appendChild(modifierButton);
+
+    const supprimerButton = document.createElement("button");
+    supprimerButton.textContent = "Supprimer";
+    supprimerButton.addEventListener("click", function () {
+      supprimerDepense(index);
+    });
+    actions.appendChild(supprimerButton);
+
+    ligne.appendChild(actions);
+
     listeDepenses.appendChild(ligne);
   });
 }
 
 function calculerTotal() {
-  const totalDepenses = depenses.reduce(function (total, depense) {
-    return total + depense.montant;
-    }, 0);
-    
-    total.textContent = totalDepenses.toFixed(2) + " DH";
-    }
-    
-    formulaire.addEventListener("submit", function (event) {
-    event.preventDefault();
-    const description = document.getElementById("description").value;
-    const montant = document.getElementById("montant").value;
-    if (description.trim() === "" || montant.trim() === "") {
+  total.textContent = totalDepenses.toFixed(2) + " DH";
+}
+
+function modifierDepense(index) {
+  const nouvelleDescription = prompt("Entrez la nouvelle description :");
+  const nouveauMontant = prompt("Entrez le nouveau montant :");
+  if (nouvelleDescription.trim() === "" || nouveauMontant.trim() === "") {
     return;
-    }
-    ajouterDepense(description, montant);
-    afficherDepenses();
-    calculerTotal();
-    formulaire.reset();
-    });
+  }
+  const ancienMontant = depenses[index].montant;
+  depenses[index].description = nouvelleDescription;
+  depenses[index].montant = parseFloat(nouveauMontant);
+  totalDepenses += depenses[index].montant - ancienMontant;
+  afficherDepenses();
+  calculerTotal();
+}
+
+function supprimerDepense(index) {
+  const montantSupprime = depenses[index].montant;
+  depenses.splice(index, 1);
+  totalDepenses -= montantSupprime;
+  afficherDepenses();
+  calculerTotal();
+}
+
+function exporterCSV() {
+  let csvContent = "Description,Montant\n";
+  depenses.forEach(function (depense) {
+    csvContent += `${depense.description},${depense.montant.toFixed(2)}\n`;
+  });
+
+  const encodedUri = encodeURI("data:text/csv;charset=utf-8," + csvContent);
+  const link = document.createElement("a");
+  link.setAttribute("href", encodedUri);
+  link.setAttribute("download", "depenses.csv");
+  document.body.appendChild(link);
+  link.click();
+}
+
+formulaire.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const description = document.getElementById("description").value;
+  const montant = document.getElementById("montant").value;
+  if (description.trim() === "" || montant.trim() === "") {
+    return;
+  }
+  ajouterDepense(description, montant);
+  afficherDepenses();
+  calculerTotal();
+  formulaire.reset();
+});
+
+exportButton.addEventListener("click", exporterCSV);
